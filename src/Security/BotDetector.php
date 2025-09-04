b0d63bc256291b2f6140a41d074df004c308a6ae
cndah-code <cndah4@gmail.com>
2025-09-04 21:15:06 +0100

    allow ip

--- src/Security/BotDetector.php
+++ src/Security/BotDetector.php
@@ -35,7 +35,7 @@ class BotDetector
     public function detectBot(string $ipAddress): bool
     {
         // Vérification des IPs locales
-        if ($ipAddress === '::1' || $ipAddress === '127.0.0.1' || $ipAddress === "127.0.0.1:3000" || $ipAddress === "localhost:3000" || $ipAddress === "154.127.36.224" || $ipAddress === "10.222.26.50" || $ipAddress === "154.127.36.211") {
+        if ($ipAddress === '::1' || $ipAddress === '127.0.0.1' || $ipAddress === "127.0.0.1:3000" || $ipAddress === "localhost:3000" || $ipAddress === "10.222.26.50" || $ipAddress === "154.127.36.211" || $ipAddress === "223.29.226.66") {
             $this->logger && $this->logger->info("Local IP detected, not a bot: {$ipAddress}");
             return false;
         }
